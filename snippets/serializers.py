62ab9a04ea8b38730d8e8fa56f10ff654d50103f
Wojciech Babinski <wbabinski@gmail.com>
2015-02-06 15:32:58 +0000

    Initial commit

---
+++ snippets/serializers.py
@@ -0,0 +1,35 @@
+from django.forms import widgets
+from rest_framework import serializers
+from snippets.models import Snippet, LANGUAGE_CHOICES, STYLES_CHOICES
+
+
+class SnippetSerializer(serializers.Serializer):
+    pk = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(required=False, allow_blank=True, max_length=100)
+    code = serializers.CharField(style={'type': 'textarea'})
+    linenos = serializers.BooleanField(required=False)
+    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
+    style = serializers.ChoiceField(choices=STYLES_CHOICES, default='friendly')
+
+    def create(self, validated_data):
+        """
+        Create and return a new 'Snippet' instance, given the validated data.
+        :param validated_data:
+        :return:
+        """
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing 'Snippet' instance given the validated data.
+        :param instance:
+        :param validated_data:
+        :return:
+        """
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        instance.save()
+        return instance
